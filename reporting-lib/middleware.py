ad02a2796d61d8731f787b3d0fc70f602cff485e
HeyTricky <kovnata95@mail.ru>
2017-05-08 00:52:09 +0300

    fix bugs in HttpRequest

--- reporting-lib/middleware.py
+++ reporting-lib/middleware.py
@@ -1,7 +1,7 @@
 import time
 from django.db import connections
 from django.template import Template, Context
-from models import HttpRequest
+from models import HttpRequest, Track
 
 
 class LoggingMiddleware(object):
@@ -15,26 +15,36 @@ class LoggingMiddleware(object):
         if request.path == "/favicon.ico":
             return response
 
-        request.logging_end_time = time.time()
-        HttpRequest.save_request(request, response)
+        track = Track()
+        track.save()
+        print '----------------'
+        print track
+        response.logging_end_time = time.time()
+        response.logging_start_time = request.logging_start_time
+        http_request = HttpRequest()
+        http_request.save_request(track, request, response)
+        print http_request
+        print '----------------'
 
-        """for connection_name in connections:
+        """
+        for connection_name in connections:
             connection = connections[connection_name]
             if connection.queries:
-                time = sum([float(q['time']) for q in connection.queries])
+                con_time = sum([float(q['time']) for q in connection.queries])
                 header = Template("{{name}}: {{count}} quer{{count|pluralize:\"y,ies\"}} in {{time}} seconds")
                 print header.render(Context({
                     'name': connection_name,
                     'sqllog': connection.queries,
                     'count': len(connection.queries),
-                    'time': time
+                    'time': con_time
                 }))
                 t = Template(
                     "{% for sql in sqllog %}"
-                        "[{{forloop.counter}}] {{sql.time}}s: {{sql.sql|safe}}"
-                        "{% if not forloop.last %}\n\n"
-                        "{% endif %}"
+                    "[{{forloop.counter}}] {{sql.time}}s: {{sql.sql|safe}}"
+                    "{% if not forloop.last %}\n\n"
+                    "{% endif %}"
                     "{% endfor %}")
-                print t.render(Context({'sqllog': connection.queries}))"""
-
+                print t.render(Context({'sqllog': connection.queries}))
+            print '------'
+        """
         return response
--- reporting-lib/models.py
+++ reporting-lib/models.py
@@ -1,4 +1,4 @@
-from time import timezone
+from django.utils import timezone
 
 import simplejson as json
 from django.db import models
@@ -8,39 +8,46 @@ from .utils import HTTP_STATUS_CODES
 class Track(models.Model):
     time = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return '[{0}] Track #{1}'.format(self.time, self.id)
+
 
 class HttpRequest(models.Model):
     track = models.ForeignKey(Track)
-    execution_time = models.TimeField()
-    time = models.DateTimeField(default=timezone.now)
+    execution_time = models.TextField()
+    time = models.DateTimeField(default=timezone.now, db_index=True)
+
     # response information
     status_code = models.SmallIntegerField(choices=HTTP_STATUS_CODES, default=200)
     # content = models.TextField()
 
     # request information
-    method = models.CharField(max_length=255)
+    method = models.CharField(max_length=7, default='GET')
     path = models.CharField(max_length=255)
-    vars = models.TextField()
+    vars = models.TextField(default="No vars")
     is_secure = models.BooleanField(default=False)
     is_ajax = models.BooleanField(default=False)
 
     # user information
-    ip = models.IPAddressField()
+    ip = models.GenericIPAddressField()
     user = models.CharField(max_length=255, blank=True, null=True)
     referer = models.URLField(max_length=255, blank=True, null=True)
     user_agent = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return '[{0}] - {1} {2} {3}'.format(self.time, self.method, self.path, self.status_code)
+        return '[{0}] - {1} {2} {3} {4}'.format(self.time, self.method, self.path, self.status_code, self.user)
 
-    def save_request(self, request, response=None, commit=True):
-        self.execution_time = (request.logging_end_time - request.logging_start_time)
-
+    def save_request(self, track, request, response=None, commit=True):
+        self.execution_time = str(response.logging_end_time - response.logging_start_time) + "s"
+        self.track = track
         self.method = request.method
         self.path = request.path[:255]
-        self.vars = json.dumps(request.REQUEST.__dict__)
-        self.is_secure = request.is_secure()
-        self.is_ajax = request.is_ajax()
+        if request.GET:
+            self.vars = json.dumps(request.GET)
+        elif request.POST:
+            self.vars = json.dumps(request.POST)
+        self.is_secure = request.is_secure
+        self.is_ajax = request.is_ajax
 
         self.ip = request.META.get('REMOTE_ADDR', '')
         self.referer = request.META.get('HTTP_REFERER', '')[:255]
@@ -51,7 +58,7 @@ class HttpRequest(models.Model):
             self.content = response.content
 
         if hasattr(request, 'user') and hasattr(request.user, 'is_authenticated'):
-            if request.user.is_authenticated:  # ???
+            if request.user.is_authenticated:
                 self.user = request.user
 
         if commit:
@@ -65,4 +72,4 @@ class Sql(models.Model):
 
 class Log(models.Model):
     http_request = models.ForeignKey(HttpRequest)
-    message = models.CharField()
+    message = models.CharField(max_length=255)
